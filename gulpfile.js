58852e0a5c937b5f6b1e91034e36c5a1554404b1
Avelino <t@avelino.xxx>
2019-05-31 17:10:01 -0300

    upgrade gulp version to ^4.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,49 @@
-const gulp = require('gulp');
-const runSequence = require('run-sequence');
-const watch = require('gulp-watch');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const notify = require('gulp-notify');
-const cssnano = require('gulp-cssnano');
-const plumber = require('gulp-plumber');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var rename = require("gulp-rename");
+var cssnano = require('gulp-cssnano');
+var plumber = require('gulp-plumber');
 
 const scssFiles = ['./static/scss/**/*.scss'];
 const scssMain = ['./static/scss/main.scss'];
 const pathStyleDest = './static/css';
 
-// SASS
-gulp.task('style', function () {
-  return gulp.src(scssMain)
+
+function styleBuildWithSourcemap () {
+  return gulp
+    .src(scssMain)
     .pipe(plumber({errorHandler: notify.onError("Style Build Error: <%= error.message %>")}))
     .pipe(sourcemaps.init())
-      .pipe(sass())
-      .pipe(autoprefixer('last 4 version'))
+    .pipe(sass())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(cssnano())
     .pipe(sourcemaps.write())
     .on('error', onError)
     .pipe(gulp.dest(pathStyleDest));
-});
+}
 
-gulp.task('style-build', function () {
-  return gulp.src(scssMain)
+function styleBuild () {
+  return gulp
+    .src(scssMain)
     .pipe(plumber({errorHandler: notify.onError("Style Build Error: <%= error.message %>")}))
     .pipe(sass())
-    .on('error', onError)
+    .pipe(rename({suffix: ".min"}))
     .pipe(autoprefixer('last 4 version'))
     .pipe(cssnano())
+    .on('error', onError)
     .pipe(gulp.dest(pathStyleDest));
-});
+}
 
-// Watcher
-gulp.task('watch', () => {
-  gulp.watch(scssFiles, function(){
-    runSequence('style', ['notify']);
-  });
-});
-
-// Build for prod
-gulp.task('build', function(callback) {
-  runSequence(['style-build'], callback)
-});
-
-// Default
-gulp.task('default', done => {
-  runSequence(['style', 'watch'], done);
-});
-
-///////////////////////////////////////////////////////////
-// Helpers
 function onError(error) {
     console.log(error.toString());
     this.emit('end');
 }
 
-gulp.task('notify', function () {
-  return gulp.src('')
-    .pipe(notify({message: 'DRAKARYS!!!', onLast: true}));
-});
+const build = gulp.series(styleBuild),
+      build_with_sourcemap = gulp.series(styleBuildWithSourcemap);
+exports.build = build;
+exports.build_with_sourcemap = build_with_sourcemap;
