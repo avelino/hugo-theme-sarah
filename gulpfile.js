042ac6a866209815db6aa9ef6c2d5120d52f6dce
Avelino <avelinorun@gmail.com>
2021-12-26 14:19:21 -0300

    upgrade packages: vulnerabilities in your dependencies
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var notify = require('gulp-notify');
@@ -12,30 +12,30 @@ const scssMain = ['./static/scss/main.scss'];
 const pathStyleDest = './static/css';
 
 
-function styleBuildWithSourcemap () {
-  return gulp
-    .src(scssMain)
-    .pipe(plumber({errorHandler: notify.onError("Style Build Error: <%= error.message %>")}))
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(autoprefixer('last 4 version'))
-    .pipe(cssnano())
-    .pipe(sourcemaps.write())
-    .on('error', onError)
-    .pipe(gulp.dest(pathStyleDest));
+function styleBuildWithSourcemap() {
+    return gulp
+        .src(scssMain)
+        .pipe(plumber({ errorHandler: notify.onError("Style Build Error: <%= error.message %>") }))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(autoprefixer('last 4 version'))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write())
+        .on('error', onError)
+        .pipe(gulp.dest(pathStyleDest));
 }
 
-function styleBuild () {
-  return gulp
-    .src(scssMain)
-    .pipe(plumber({errorHandler: notify.onError("Style Build Error: <%= error.message %>")}))
-    .pipe(sass())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(autoprefixer('last 4 version'))
-    .pipe(cssnano())
-    .on('error', onError)
-    .pipe(gulp.dest(pathStyleDest));
+function styleBuild() {
+    return gulp
+        .src(scssMain)
+        .pipe(plumber({ errorHandler: notify.onError("Style Build Error: <%= error.message %>") }))
+        .pipe(sass())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(autoprefixer('last 4 version'))
+        .pipe(cssnano())
+        .on('error', onError)
+        .pipe(gulp.dest(pathStyleDest));
 }
 
 function onError(error) {
@@ -44,6 +44,6 @@ function onError(error) {
 }
 
 const build = gulp.series(styleBuild),
-      build_with_sourcemap = gulp.series(styleBuildWithSourcemap);
+    build_with_sourcemap = gulp.series(styleBuildWithSourcemap);
 exports.build = build;
 exports.build_with_sourcemap = build_with_sourcemap;
